3c7293acf16c059a5da0b49fcdc1c83e0a4c0ecc
andres97473 <andres97473@hotmail.com>
2022-05-28 08:49:48 -0500

    Commit 2022-05-28 08:48

--- Socket.io/juego-pintura/frontend/src/app/draw/draw.component.ts
+++ Socket.io/juego-pintura/frontend/src/app/draw/draw.component.ts
@@ -27,7 +27,7 @@ export class DrawComponent implements OnInit, AfterViewInit {
 
     if (e.target.id === 'canvasId') {
       // console.log(e);
-      // this.write(e);
+      this.write(e);
     }
   };
 
@@ -59,6 +59,29 @@ export class DrawComponent implements OnInit, AfterViewInit {
       x: res.clientX - rect.left,
       y: res.clientY - rect.top,
     };
-    console.log(prevPos);
+    // console.log(prevPos);
+    this.writeSingle(prevPos);
+  }
+
+  private writeSingle = (prevPos: any) => {
+    this.points.push(prevPos);
+    if (this.points.length > 3) {
+      const prevPos = this.points[this.points.length - 1];
+      const currentPos = this.points[this.points.length - 2];
+      // console.log(prevPos, currentPos);
+      this.drawOnCanvas(prevPos, currentPos);
+    }
+  };
+
+  private drawOnCanvas(prevPos: any, currentPos: any) {
+    if (!this.cx) {
+      return;
+    }
+    this.cx.beginPath();
+    if (prevPos) {
+      this.cx.moveTo(prevPos.x, prevPos.y);
+      this.cx.lineTo(prevPos.x, prevPos.y);
+      this.cx.stroke();
+    }
   }
 }

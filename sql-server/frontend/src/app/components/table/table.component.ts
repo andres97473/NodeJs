450a5c5e35d5c40946e8457648e704d1c2e9f8ee
andres97473 <andres97473@hotmail.com>
2022-06-14 22:18:50 -0500

    Commit 2022-06-14 22:18

--- sql-server/frontend/src/app/components/table/table.component.ts
+++ sql-server/frontend/src/app/components/table/table.component.ts
@@ -18,6 +18,7 @@ export class TableComponent implements OnInit {
   dataSource: HistoriaI[] = [];
 
   texto1: string[] = [];
+  texto2: string[] = [];
 
   selectedRow: HistoriaI = {
     no_historia: '',
@@ -41,18 +42,61 @@ export class TableComponent implements OnInit {
     console.log(this.selectedRow);
 
     if (this.selectedRow.texto01) {
-      const splits = this.splitString(this.selectedRow.texto01, /100|124/);
+      const txt1 = this.splitString(
+        this.selectedRow.texto01,
+        /1001 |1002 |1003 |1004 |1005 |1006 |1007 |1008 |1009 |1010 |1030 |1031 |1036 |1350 |1242 |1243 |1244|1245 /
+      );
 
-      this.texto1 = splits;
+      this.texto1 = txt1;
     }
-    console.log(this.texto1);
+
+    if (this.selectedRow.texto02) {
+      const txt2 = this.splitString(
+        this.selectedRow.texto02,
+        /-400 |-401 |-402 |-403 |-404 |-405 |-406 |-407 |-408 |-409 |-410 |-411 |8100 |8000 |8001 |8002 |8003 | 8004|8005 |8006 |8007 /
+      );
+      this.texto2 = txt2;
+    }
+
+    for (const iterator of this.texto1) {
+      console.log(this.convertirString(iterator));
+    }
+
+    // console.log(this.texto1);
+    // console.log(this.texto2);
   }
 
   splitString(string: string, separator: any = ' ') {
     const split = string.split(separator);
-    console.log(split.length);
 
     return split;
+  }
+
+  convertirString(string: string) {
+    const split = string.split(':');
+
+    const obj = {
+      largo: split[0].trim().split(' ')[0],
+      orden: split[0].trim().split(' ')[1],
+      codigo: split[0],
+      cuerpo: '',
+    };
+
+    for (var i = 1; i < split.length; i++) {
+      // console.log(split[i]);
+      obj.cuerpo += split[i] + ':';
+    }
+
+    let cuerpo = obj.cuerpo;
+    let orden = obj.orden || '';
+    cuerpo = cuerpo.substring(0, cuerpo.length - 1);
+    orden = orden.substring(0, 2);
+    obj.cuerpo = cuerpo;
+    obj.orden = orden;
+    // console.log(cuerpo);
+    obj.codigo = obj.codigo.split(obj.orden)[1];
+
+    return obj;
   }
 
   convertirFecha(date: string) {

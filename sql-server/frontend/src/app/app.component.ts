439ddad1aecf9ba4c65b01db4425a275c83ed738
andres97473 <andres97473@hotmail.com>
2022-07-01 20:45:25 -0500

    Commit 2022-07-01 20:45

--- sql-server/frontend/src/app/app.component.ts
+++ sql-server/frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    console.log(this.document.location.href);
+  }
 }

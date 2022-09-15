ddf0fff8482c18fa968c163ec7c5babba6bd29fc
andres97473 <andres97473@hotmail.com>
2022-09-15 16:50:48 -0500

    Commit 2022-09-15 16:50

--- whatsapp-bot-master/adminpro/src/app/pages/mensajes-prueba/mensajes-prueba.component.ts
+++ whatsapp-bot-master/adminpro/src/app/pages/mensajes-prueba/mensajes-prueba.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Usuario } from '../../models/usuario.model';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-mensajes-prueba',
   templateUrl: './mensajes-prueba.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MensajesPruebaComponent implements OnInit {
+  public usuario: Usuario;
+  public pruebaForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) {
+    this.usuario = usuarioService.usuario;
+    this.iniciarFormulario();
   }
 
+  ngOnInit(): void {}
+
+  iniciarFormulario() {
+    this.pruebaForm = this.fb.group({
+      token: [this.usuario.uid, [Validators.required]],
+      celular: [this.usuario.celular],
+      repeticiones: [1],
+      mensaje: [this.usuario.nombre, [Validators.required]],
+    });
+  }
+
+  sendPrueba() {
+    console.log(this.pruebaForm.value);
+  }
 }

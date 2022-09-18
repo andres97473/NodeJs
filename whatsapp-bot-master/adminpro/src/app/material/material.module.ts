f7788947b41e44453dc22fc7219ec38c69d41e0c
andres97473 <andres97473@hotmail.com>
2022-09-18 13:58:59 -0500

    Commit 2022-09-18 13:58

--- whatsapp-bot-master/adminpro/src/app/material/material.module.ts
+++ whatsapp-bot-master/adminpro/src/app/material/material.module.ts
@@ -6,6 +6,9 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [],
@@ -15,12 +18,18 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatInputModule,
     MatIconModule,
     MatAutocompleteModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   exports: [
     MatChipsModule,
     MatInputModule,
     MatIconModule,
     MatAutocompleteModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
 })
 export class MaterialModule {}
--- whatsapp-bot-master/adminpro/src/app/pages/mensajes-archivo/mensajes-archivo.component.ts
+++ whatsapp-bot-master/adminpro/src/app/pages/mensajes-archivo/mensajes-archivo.component.ts
@@ -1,23 +1,77 @@
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+//chips
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
+// table
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
+import * as XLSX from 'xlsx';
+
+// interfaces
 export interface Celular {
   numero: string;
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+];
 
 @Component({
   selector: 'app-mensajes-archivo',
   templateUrl: './mensajes-archivo.component.html',
   styleUrls: ['./mensajes-archivo.component.css'],
 })
-export class MensajesArchivoComponent {
+export class MensajesArchivoComponent implements AfterViewInit {
+  // propiedades
+  celulares: Celular[] = [];
+  excelData: any;
+  titulos: any;
+  // chips
   addOnBlur = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  celulares: Celular[] = [];
+  // tables
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 
   add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
+    const value = (event.value || '').trim().replace(/ /g, '');
+    console.log(value);
 
     // Add our fruit
     if (value) {
@@ -37,6 +91,26 @@ export class MensajesArchivoComponent {
   }
 
   verCelulares() {
-    console.log(this.celulares);
+    this.celulares.push(
+      { numero: '310' },
+      { numero: '312' },
+      { numero: '315' }
+    );
+  }
+
+  readExcel(evento: any) {
+    let file = evento.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e) => {
+      let workBook = XLSX.read(fileReader.result, { type: 'binary' });
+      let sheetNames = workBook.SheetNames;
+
+      this.excelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+      this.titulos = Object.keys(this.excelData[0]);
+      console.log(this.excelData);
+      console.log(Object.keys(this.excelData[0]));
+    };
   }
 }

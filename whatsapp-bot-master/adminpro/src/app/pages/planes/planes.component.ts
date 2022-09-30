c6cd6a6114581c1ccecae8ae8a445274e7a67379
andres97473 <andres97473@hotmail.com>
2022-09-30 12:01:42 -0500

    Commit 2022-09-30 12:01

--- whatsapp-bot-master/adminpro/src/app/pages/planes/planes.component.ts
+++ whatsapp-bot-master/adminpro/src/app/pages/planes/planes.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { PlanesService } from '../../services/planes.service';
+import { Plan } from '../../interface/plan.interface';
 
 @Component({
   selector: 'app-planes',
   templateUrl: './planes.component.html',
-  styleUrls: ['./planes.component.css']
+  styleUrls: ['./planes.component.css'],
 })
 export class PlanesComponent implements OnInit {
+  planes: Plan[] = [];
 
-  constructor() { }
+  constructor(private planesService: PlanesService) {}
 
   ngOnInit(): void {
+    this.cargarPlanes();
   }
 
+  cargarPlanes() {
+    this.planesService.getPlanes().subscribe((resp: any) => {
+      console.log(resp.planes);
+      this.planes = resp.planes;
+    });
+  }
+
+  solicitarPlan(plan: Plan) {
+    console.log(plan);
+  }
 }

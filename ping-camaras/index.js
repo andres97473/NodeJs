16d2c36414d67a6d9f3be3f8819f18c96fba0529
andres97473 <andres97473@hotmail.com>
2022-11-30 16:34:38 -0500

    Commit 2022-11-30 16:34

--- ping-camaras/index.js
+++ ping-camaras/index.js
@@ -1,14 +1,68 @@
 require("dotenv").config();
 
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth, MessageMedia, List } = require("whatsapp-web.js");
+const moment = require("moment");
+
 const cron = require("node-cron");
 const exec = require("child_process").exec;
 const fs = require("fs");
 
+// constantes
 const host = process.env.HOST;
+const codeNumber = process.env.NUMBER_CODE;
+const phoneNumber = process.env.NUMBER_PHONE;
 const hosts = [host];
 const replyFromLocale = "Respuesta desde";
-
 const promises = [];
+
+const msg = "Chat iniciado!";
+const formatDate = "DD/MM/YYYY hh:mm a";
+const fech = moment().format(formatDate);
+const inicioFech = msg + " " + fech;
+
+// cleinte whatsapp
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+  },
+});
+
+client.initialize();
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("authenticated", () => {
+  console.log("AUTHENTICATED");
+});
+
+client.on("auth_failure", (msg) => {
+  // Fired if session restore was unsuccessfull
+  console.error("AUTHENTICATION FAILURE", msg);
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+
+  // Funcion de prueba a mi propio numero
+  setTimeout(() => {
+    let chatId = `${process.env.NUMBER_CODE}${process.env.NUMBER_PHONE}@c.us`;
+
+    client.sendMessage(chatId, inicioFech).then((response) => {
+      if (response.id.fromMe) {
+        console.log("It works! " + inicioFech);
+        // correr app desde navegador
+        // Abrir con navegador predeterminado
+        // TODO: abrir con navegador predeterminado
+        //c.exec("start http://localhost:3000/#/");
+      }
+    });
+  }, 500);
+});
 
 function pingHost() {
   hosts.forEach((host) => {
@@ -26,6 +80,11 @@ function pingHost() {
             output.substring(replyFromIndex).toUpperCase().indexOf("BYTES") > 0
           ) {
             status = "CONECTADO";
+          } else {
+            console.log("Desconectado!!");
+            // TODO: whatsapp
+            let chatId = `${process.env.NUMBER_CODE}${process.env.NUMBER_PHONE}@c.us`;
+            client.sendMessage(chatId, "Desconectado!!");
           }
           const fecha = new Date();
           resolve(

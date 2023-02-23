30fea0e37fff7dc33bac4b01cc5504a60f339231
andres97473 <andres97473@hotmail.com>
2023-02-23 15:53:26 -0500

    Commit 2023-02-23 15:53

--- prueba-wweb/promises/promesas.js
+++ prueba-wweb/promises/promesas.js
@@ -33,23 +33,24 @@ module.exports = {
   getCitasDisponibles,
 };
 
-// getCitasDisponibles("2023-02-09")
-//   .then((res) => {
-//     for (const profesionales of res) {
-//       console.log(profesionales.profesional);
-
-//       for (const cita of profesionales.disponibles) {
-//         console.log(cita);
-//       }
-//     }
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 getCitasDisponibles("2023-02-09")
   .then((res) => {
-    console.log(res);
+    for (const profesionales of res) {
+      console.log(profesionales.profesional);
+
+      for (const cita of profesionales.disponibles) {
+        console.log(cita);
+      }
+    }
   })
   .catch((err) => {
     console.log(err);
   });
+
+// getCitasDisponibles("2023-02-09")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
